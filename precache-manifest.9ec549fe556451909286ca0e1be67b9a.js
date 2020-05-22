2e0e98b672ef5c4bba0b5a408d69d38381e29326
TipaMag <perynsky@gmail.com>
2020-05-22 23:08:59 +0300

    Updates

---
+++ precache-manifest.9ec549fe556451909286ca0e1be67b9a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48c071cab5364e528823204e753defc",
+    "url": "/abz-agency-test-task/index.html"
+  },
+  {
+    "revision": "c12a6bcb8eaaed319f3d",
+    "url": "/abz-agency-test-task/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "26c1f4f7a6cc80dd6e5f",
+    "url": "/abz-agency-test-task/static/css/main.2681a656.chunk.css"
+  },
+  {
+    "revision": "c12a6bcb8eaaed319f3d",
+    "url": "/abz-agency-test-task/static/js/2.04612250.chunk.js"
+  },
+  {
+    "revision": "28164e9e1f7b49ce7422bf240655ba5b",
+    "url": "/abz-agency-test-task/static/js/2.04612250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c1f4f7a6cc80dd6e5f",
+    "url": "/abz-agency-test-task/static/js/main.0a5d12bd.chunk.js"
+  },
+  {
+    "revision": "4b15f19880471c2d7584",
+    "url": "/abz-agency-test-task/static/js/runtime-main.0d3007a6.js"
+  },
+  {
+    "revision": "5d5735e57127db2f7a2ad879fc6056b8",
+    "url": "/abz-agency-test-task/static/media/OpenSans-Regular.5d5735e5.woff2"
+  },
+  {
+    "revision": "6fde2eb6728eb97fd914dcd0641d332b",
+    "url": "/abz-agency-test-task/static/media/OpenSans-Regular.6fde2eb6.woff"
+  },
+  {
+    "revision": "21c776365beba0f1ff78315711b43aea",
+    "url": "/abz-agency-test-task/static/media/PTSans-Regular.21c77636.woff2"
+  },
+  {
+    "revision": "8f69eec893df9a78a9695b3ce4d349f9",
+    "url": "/abz-agency-test-task/static/media/PTSans-Regular.8f69eec8.woff"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abz-agency-test-task/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abz-agency-test-task/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abz-agency-test-task/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/abz-agency-test-task/static/media/menu-icon.6f6bef28.svg"
+  },
+  {
+    "revision": "3a41f082d05e2a2756c67f67f21b2324",
+    "url": "/abz-agency-test-task/static/media/photo-cover.3a41f082.svg"
+  }
+]);
